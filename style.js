8ac1d48ae6bc7b2733f56f81a5924fe6de9e266d
MarwaHafiz <46488700+MarwaHafiz@users.noreply.github.com>
2019-01-08 23:33:30 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,16 @@
+/*var nam= document.getElementById("hi");
+function login(){
+	if(nam.value ==""){
+		alert("hello user");
+	}
+	else{
+		alert("hello " + nam.value);
+	}
+}*/
+var myUl = document.getElementById("mylist");
+var lis = "";
+for(var i =0 ;i<20; i++){
+	lis=lis+"<li>test</li>";
+}
+
+mylist.innerHTML = lis;
